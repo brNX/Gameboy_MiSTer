332cbc0ef5fa5edd3c316cf6e6b430f7fc6268c8
Bruno Duarte Gouveia <bgouveia@gmail.com>
2019-05-27 17:46:34 +0100

    added sdl timer to trigger the verilator tick

--- gb_tb.cpp
+++ gb_tb.cpp
@@ -10,6 +10,27 @@
 #include "imgui_sdl.h"
 
 
+Uint32 timerTick(Uint32 interval, void *param)
+{
+    SDL_Event event;
+    SDL_UserEvent userevent;
+
+    /* In this example, our callback pushes an SDL_USEREVENT event
+    into the queue, and causes our callback to be called again at the
+    same interval: */
+
+    userevent.type = SDL_USEREVENT;
+    userevent.code = 0;
+    userevent.data1 = NULL;
+    userevent.data2 = NULL;
+
+    event.type = SDL_USEREVENT;
+    event.user = userevent;
+
+    SDL_PushEvent(&event);
+    return(interval);
+}
+
 int main(int argc, char **argv) {
 	
     // Setup SDL
@@ -33,22 +54,15 @@ int main(int argc, char **argv) {
     // initialize simulation inputs
     top->clk_sys = 1;
     top->reset = 1;
+    top->ce = 1;
+    top->ce_2x = 1;
     top->gb->vram0_array[0] = 0x55;
     top->gb->vram1_array[0] = 0x55;
     top->eval ();
     
-    // run simulation for 100 clock periods
-    for (i=0; i<20; i++) {
-      top->reset = (i < 2);
-      // dump variables into VCD file and toggle clock
-      for (clk=0; clk<2; clk++) {
-        tfp->dump (2*i+clk);
-        top->clk_sys = !top->clk_sys;
-        top->eval ();
-      }
-      if (Verilated::gotFinish())  exit(0);
-    }
-    tfp->close();
+
+    Uint32 delay = (33 / 10) * 10;  /* To round it down to the nearest 10 ms */
+    SDL_TimerID my_timer_id = SDL_AddTimer(delay, timerTick, NULL);
 
     SDL_Window* window = SDL_CreateWindow("SDL2 ImGui Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -74,6 +88,8 @@ int main(int argc, char **argv) {
     
 
 	bool run = true;
+    bool isGBC = false;
+    bool runVerilator = false;
 	while (run)
 	{
 		ImGuiIO& io = ImGui::GetIO();
@@ -83,19 +99,41 @@ int main(int argc, char **argv) {
 		SDL_Event e;
 		while (SDL_PollEvent(&e))
 		{
-			if (e.type == SDL_QUIT) run = false;
-			else if (e.type == SDL_WINDOWEVENT)
-			{
-				if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
-				{
-					io.DisplaySize.x = static_cast<float>(e.window.data1);
-					io.DisplaySize.y = static_cast<float>(e.window.data2);
-				}
-			}
-			else if (e.type == SDL_MOUSEWHEEL)
-			{
-				wheel = e.wheel.y;
-			}
+
+            switch(e.type)
+            {            
+                case SDL_QUIT:
+                        run = false;
+                        break;
+
+                case SDL_WINDOWEVENT:
+                    if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+                    {
+                        io.DisplaySize.x = static_cast<float>(e.window.data1);
+                        io.DisplaySize.y = static_cast<float>(e.window.data2);
+                    }
+                    break;
+                    
+                case SDL_MOUSEWHEEL:
+                    wheel = e.wheel.y;
+                    break;
+    
+                case SDL_USEREVENT: {
+                    /* and now we can call the function we wanted to call in the timer but couldn't because of the multithreading problems */
+                    if (runVerilator){
+                        i++;
+                        top->reset = (i < 2);
+                        // dump variables into VCD file and toggle clock
+                        for (clk=0; clk<2; clk++) {
+                            tfp->dump (2*i+clk);
+                            top->clk_sys = !top->clk_sys;
+                            top->eval ();
+                        }
+                        if (Verilated::gotFinish()) run = false;
+                    }
+                    break;
+                }
+            }
 		}
 
 		int mouseX, mouseY;
@@ -117,6 +155,12 @@ int main(int argc, char **argv) {
 		ImGui::Image(texture, ImVec2(100, 100));
 		ImGui::End();
 
+        ImGui::Begin("GB Config");
+        ImGui::Checkbox("Enable Gameboy Color?",&isGBC);
+        ImGui::Checkbox("Run?",&runVerilator);
+        ImGui::LabelText("Cycles Elapsed","%d",i+2);
+        ImGui::End();
+
 		SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
 		SDL_RenderClear(renderer);
 
@@ -125,6 +169,9 @@ int main(int argc, char **argv) {
 
 		SDL_RenderPresent(renderer);
 	}
+
+    
+    tfp->close();
 
 	ImGuiSDL::Deinitialize();
 
