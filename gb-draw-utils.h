a94a8ac7636da9b600e8a41c6271e3853f985d32
Bruno Duarte Gouveia <bgouveia@gmail.com>
2019-06-06 15:46:47 +0100

    refactoring

--- gb-draw-utils.h
+++ gb-draw-utils.h
@@ -5,10 +5,17 @@
 #include "Vgb.h"
 #include "Vgb_gb.h"
 #include "Vgb_video.h"
+#include "Vgb_lcd.h"
+#include "Vgb_sprite.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
 SDL_Color getColor(int number,int mode, Vgb_video* video);
 void loadvram(char * fileName, CData * array) ;
+void drawBackground(SDL_Texture* background, SDL_Renderer* renderer, Vgb* top);
+void drawTileMap(SDL_Texture* tilemap, SDL_Renderer* renderer, Vgb* top);
+void drawSprite(SDL_Texture* sprite_texture, SDL_Renderer* renderer, Vgb_sprite* sprite,Vgb* top, bool isGBC);
+void drawLCD(SDL_Texture* tilemap, SDL_Renderer* renderer, Vgb* top, bool isGBC);
+
 
 #endif // GBDRAWUTILS_H
