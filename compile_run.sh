2010357c893d37f8ad099c4dc832ac0e042b2a4e
Bruno Duarte Gouveia <bgouveia@gmail.com>
2019-06-08 22:35:50 +0100

    started adding the rest of the core

--- compile_run.sh
+++ compile_run.sh
@@ -6,10 +6,10 @@ rm -f  gb.vcd
 
 
 # run Verilator to translate Verilog into C++, include C++ testbench
-verilator -O2 -LDFLAGS  "-lgdi32 -lopengl32 -limm32 \`pkg-config --static --libs sdl2\` -IC:/verilator/imgui -IC:/verilator/imgui/examples" -CFLAGS "\`pkg-config --cflags sdl2\` -IC:/verilator/imgui -IC:/verilator/imgui/examples -fpermissive" -Wall --cc --trace gb.v --exe gb_tb.cpp imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_widgets.cpp imgui/imgui_sdl.cpp imgui/imgui_demo.cpp gb-draw-utils.cpp
+verilator -y tv80 -O2 -LDFLAGS  "-lgdi32 -lopengl32 -limm32 \`pkg-config --static --libs sdl2\` -IC:/verilator/imgui -IC:/verilator/imgui/examples" -CFLAGS "\`pkg-config --cflags sdl2\` -IC:/verilator/imgui -IC:/verilator/imgui/examples -fpermissive" -Wall --cc --trace Gameboy.sv --exe gb_tb.cpp imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_widgets.cpp imgui/imgui_sdl.cpp imgui/imgui_demo.cpp gb-draw-utils.cpp
 # build C++ project
-make -j -C obj_dir/ -f Vgb.mk Vgb
+make -j -C obj_dir/ -f VGameboy.mk VGameboy
 # run executable simulation
-obj_dir/Vgb
+obj_dir/VGameboy
 
 # gtkwave gb.vcd gb.sav &
